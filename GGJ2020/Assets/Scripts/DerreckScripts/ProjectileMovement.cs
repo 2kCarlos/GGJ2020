30d1ead6908c00d54e20f103b7ddcea5952af65e
Derreck O'Neil <derreck_oneil@bloomfield.edu>
2020-02-01 16:26:11 -0500

    PS system/ bullet destroys after duration

---
+++ GGJ2020/Assets/Scripts/DerreckScripts/ProjectileMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace GGJ2020
+{
+    public class ProjectileMovement : MonoBehaviour
+    {
+        [SerializeField] private int moveSpeed;
+        [SerializeField] private ParticleSystem ps;
+        Transform pos;
+        
+        private void Update()
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            StartCoroutine(DestroyBullet());
+        }
+        System.Collections.IEnumerator DestroyBullet()
+        {
+            yield return new WaitForSeconds(3);
+            Destroy(gameObject);
+        }
+        
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "Knight")
+            {
+                
+                Destroy(collision.gameObject);
+                Instantiate(ps, collision.transform.position, collision.transform.rotation);
+                
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
