eb6c42dd235f2f37fc903e44a05929bf7e4a2557
Carlos <modlunar@hotmail.com>
2020-02-01 00:44:37 -0500

    Demo of moving character in my test scene

---
+++ GGJ2020/Assets/Scripts/Movement/ThirdPersonMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+using static Unity.Mathematics.math;
+
+namespace GGJ2020 {
+	public class ThirdPersonMovement : MonoBehaviour {
+		[SerializeField] private PlayerInput playerInput;
+		[SerializeField] private new Rigidbody rigidbody;
+		[SerializeField] private new Collider collider;
+		[Space(20)]
+		[SerializeField] private ThirdPersonMovementSettings settings;
+
+		private float2 inputMove;
+		private float inputMoveSqrMag;
+		private float3 desiredVelocity;
+		private bool isMoving;
+
+		#region Unity Messages
+		private void FixedUpdate() {
+			float3 velocity = rigidbody.velocity;
+			desiredVelocity = new float3(inputMove.x, 0, inputMove.y);
+			if (isMoving) {
+				//Dividing by the length here (aka dividing by the sqrt of the squared magnitude)
+				//normalizes the vector without needing additional calculations
+				desiredVelocity = settings.MoveSpeed * (desiredVelocity / sqrt(inputMoveSqrMag));
+
+			}
+
+			rigidbody.AddForce(desiredVelocity - velocity, ForceMode.VelocityChange);
+		}
+		#endregion
+
+
+		#region InputSystem Messages
+		private void OnMove(InputValue value) {
+			inputMove = value.Get<Vector2>();
+			inputMoveSqrMag = lengthsq(inputMove);
+			isMoving = inputMoveSqrMag >= 0.01f;
+			collider.material = (isMoving) ? settings.MovingPhysicMaterial : settings.NonMovingPhysicMaterial;
+		}
+		#endregion
+	}
+}
---
+++ GGJ2020/Assets/Scripts/Movement/ThirdPersonMovementSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace GGJ2020 {
+	[CreateAssetMenu]
+	public class ThirdPersonMovementSettings : ScriptableObject {
+		[SerializeField] private PhysicMaterial movingPhysicMaterial;
+		[SerializeField] private PhysicMaterial nonMovingPhysicMaterial;
+		[SerializeField] private float moveSpeed = 10;
+
+		public float MoveSpeed {
+			get { return moveSpeed; }
+		}
+
+		public PhysicMaterial MovingPhysicMaterial {
+			get { return movingPhysicMaterial; }
+		}
+
+		public PhysicMaterial NonMovingPhysicMaterial {
+			get { return nonMovingPhysicMaterial; }
+		}
+	}
+}
