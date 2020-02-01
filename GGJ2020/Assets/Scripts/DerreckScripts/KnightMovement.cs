1b7ad6a2a82a46d21930a83119b4e9d371dd4b40
Derreck O'Neil <derreck_oneil@bloomfield.edu>
2020-01-31 23:12:52 -0500

    Princess and Knight saved

---
+++ GGJ2020/Assets/Scripts/DerreckScripts/KnightMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+namespace GGJ2020
+{
+    public class KnightMovement : MonoBehaviour
+    {
+        GameObject Player;
+        void Start()
+        {
+            Player = GameObject.FindWithTag("Player");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.LookAt(Player.transform);
+
+        }
+        
+    }
+}
---
+++ GGJ2020/Assets/Scripts/DerreckScripts/PrincessStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+namespace GGJ2020
+{
+    public class PrincessStats : MonoBehaviour
+    {
+        void Update()
+        {
+
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Knight")
+            {
+                Debug.Log("Hit by the knight!");
+            }
+        }
+    }
+}
