127b520ef771d191822d966a7cd828e75d0f2ac6
Carlos <modlunar@hotmail.com>
2020-02-01 08:46:12 -0500

    Beginning to define a set of persistent GameObjects with SOLID code

---
+++ GGJ2020/Assets/Scripts/CarlosScripts/PersistentObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GGJ2020 {
+	public class PersistentObject : MonoBehaviour {
+		[SerializeField] private PersistentObjectSet set;
+
+		#region Unity Messages
+		private void OnEnable() {
+			set.Add(this);
+		}
+
+		private void OnDisable() {
+			set.Remove(this);
+		}
+		#endregion
+	}
+}
---
+++ GGJ2020/Assets/Scripts/CarlosScripts/RuntimeSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ2020 {
+	public abstract class RuntimeSet<T> : RuntimeSetBase {
+		[SerializeField] private List<T> list = new List<T>();
+
+		//Would prefer to use Unity 2020.1+ generic serialization with UnityEvent<T> in the inspector, but this will do I suppose!
+		public event Action<T> onAdd;
+		public event Action<T> onRemove;
+
+		public void Add(T obj) {
+			list.Add(obj);
+			if (onAdd != null)
+				onAdd(obj);
+		}
+
+		public bool Remove(T obj) {
+			bool result = list.Remove(obj);
+			if (onRemove != null)
+				onRemove(obj);
+			return result;
+		}
+	}
+}
