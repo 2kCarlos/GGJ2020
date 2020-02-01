f0ac7d729d4166b91c6a6d42be6747a94e358151
Derreck O'Neil <derreck_oneil@bloomfield.edu>
2020-01-31 22:36:00 -0500

    Bullet Prefab implementation

---
+++ GGJ2020/Assets/Scripts/DerreckScripts/BulletMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GGJ2020
+{
+    public class BulletMovement : MonoBehaviour
+    {
+        [SerializeField] private int moveSpeed;
+        private void Update()
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+}
+
