a760cffaaab345b35ae9ba202496964191e50ce6
Carlos <modlunar@hotmail.com>
2020-02-02 16:52:08 -0500

    Last fixes for building for Windows 64bit

---
+++ GGJ2020/Assets/Scripts/CarlosScripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GGJ2020 {
+	public class SceneLoader : MonoBehaviour {
+		[SerializeField] private int sceneBuildIndex = 0;
+		[SerializeField] private LoadSceneMode loadSceneMode;
+		[SerializeField] private bool setAsActive = true;
+
+		#region Unity Messages
+		private void Awake() {
+			if (!SceneManager.GetSceneByBuildIndex(sceneBuildIndex).IsValid()) {
+				AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneBuildIndex, loadSceneMode);
+				if (setAsActive) {
+					loadOperation.completed += (AsyncOperation a) => {
+						Scene s = SceneManager.GetSceneByBuildIndex(sceneBuildIndex);
+						SceneManager.SetActiveScene(s);
+					};
+				}
+			}
+		}
+		#endregion
+	}
+}
