73f9a8930e1cf1db3454ea0ac6078e3c54644b45
Derreck O'Neil <derreck_oneil@bloomfield.edu>
2020-02-02 11:02:46 -0500

    Bullet logic changes

---
+++ GGJ2020/Assets/Scripts/DerreckScripts/KeyFragmentSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+namespace GGJ2020
+{
+    public class KeyFragmentSpawn : MonoBehaviour
+    {
+        [SerializeField] private GameObject KeyFragment;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "Bullet")
+            {
+                Instantiate(KeyFragment, transform.position, transform.rotation);
+            }
+        }
+    }
+}
--- GGJ2020/Assets/Scripts/SampleScript.cs
+++ GGJ2020/Assets/Scripts/SampleScript.cs
@@ -1,7 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace GGJ2020 {
 	public class SampleScript : MonoBehaviour {
 
-	}
+        delegate int incrInt(int j);
+        //delegate int Plle();
+		private void Start()
+		{
+            #region LamdaOp
+            int[] a = { 1, 2, 3, 4 };
+            
+            //int doubleTheInt = 
+            //var square = numbers.Select(x => x * x);
+
+            #endregion
+        }
+        private string returnThis(string message)
+        {
+            return message;
+        }
+    }
 }
